41c2eddc5a9ee928a90d983de74ae15373c2e5cc
Omkarmakani0003 <makani.omkar3141@gmail.com>
2026-03-13 18:06:55 +0530

    todo app complated

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -15,15 +15,24 @@ function App() {
      setTodo((prev)=> prev.map((prevTodo)=> ( prevTodo.id == id ? todo : prevTodo)))
   }
   const deleteTodo = (id)=>{
-    
+    setTodo((prev)=> prev.filter((prevTodo)=> ( prevTodo.id != id)))
   }
 
-  const complated = (id)=>{
-
+  const complated = (id)=>{ 
+    console.log(id)
+    setTodo((prev)=> prev.map((prevTodo)=> prevTodo.id == id ? {...prevTodo, 'complated': !prevTodo.complated } : prevTodo)) 
   }
-  // useEffect(()=>{
-    
-  // },[])
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem('todos')) 
+    if(todos && todos.length > 0){
+      setTodo(todos)
+    }
+  },[])
+
+  useEffect(()=>{
+      localStorage.setItem('todos',JSON.stringify(todos))
+  },[todos])
 
   return (
     <TodoProvider value={{todos,addTodo,editTodo,deleteTodo,complated}}>
@@ -31,8 +40,9 @@ function App() {
         <h1 className="title">Todo List</h1>
         <Form />
         <ul className="todo-list">
+
           {todos.map((todo)=>(
-            <li className="todo-item" key={todo.id} >
+            <li className="todo-item" key={todo.id} style={todo.complated ? {background:'#6bd96b'}:{background:'#858585'}}>
               <Item todo={todo}/>
             </li>
           ))}
--- Todo/src/components/Item.jsx
+++ Todo/src/components/Item.jsx
@@ -8,27 +8,36 @@ function Item({todo}){
     const [task,setTask] = useState(todo.todo)
 
     const edit = ()=>{
+       if(todo.complated) return
        setTask(todo.todo)
        setEditable(!editable)
     }
     const save = ()=>{
-        if(!task) return
+        if(!task || todo.complated) return
         editTodo(todo.id,{...todo,todo:task})
         setEditable(!editable)
+    }
+
+    const remove = ()=>{
+        deleteTodo(todo.id)
+    }
+
+    const check = ()=>{
+        console.log(todo.id)
+        complated(todo.id)
     }
 
     return (
         <>
             <div className="left">
-              <input type="checkbox" className="check" />
+              <input type="checkbox" className="check" onChange={check} checked={todo.complated}/>
               <input type="text" className="todo-input" value={task} onChange={(e)=>{setTask(e.target.value)}} disabled={editable}/>
-              {/* <span className="task">{todo.todo}</span> */}
             </div>
 
             <div className="actions">
 
               <button className="edit-btn" onClick={editable ? edit : save}>{editable ? 'Edit' : 'Save'}</button>
-              <button className="delete-btn">Delete</button>
+              <button className="delete-btn" onClick={remove}>Delete</button>
             </div>
         </>
     )
